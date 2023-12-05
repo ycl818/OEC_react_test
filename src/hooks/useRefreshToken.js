94c8cbd438de007486d5d1493ac566650f2e5516
ycl818 <kevinyichienlee@gmail.com>
2023-12-05 15:12:52 +0800

    add trycatch in useRefreshToken

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -5,21 +5,28 @@ const useRefreshToken = () => {
   const { setAuth } = useAuth();
 
   const refresh = async () => {
-    const response = await axios.get("/refresh", {
-      withCredentials: true,
-    });
-    console.log("file: useRefreshToken.js:12 ~ refresh ~ response:", response);
-    setAuth((prev) => {
-      console.log("prevState: ", JSON.stringify(prev));
-      console.log(response.data.accessToken);
-      return {
-        ...prev,
-        accessToken: response.data.accessToken,
-        user: response.data.username,
-      };
-    });
+    try {
+      const response = await axios.get("/refresh", {
+        withCredentials: true,
+      });
+      console.log(
+        "file: useRefreshToken.js:12 ~ refresh ~ response:",
+        response
+      );
+      setAuth((prev) => {
+        console.log("prevState: ", JSON.stringify(prev));
+        console.log(response.data.accessToken);
+        return {
+          ...prev,
+          accessToken: response.data.accessToken,
+          user: response.data.username,
+        };
+      });
 
-    return response.data.accessToken;
+      return response.data.accessToken;
+    } catch (error) {
+      console.log(error);
+    }
   };
   return refresh;
 };
